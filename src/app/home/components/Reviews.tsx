db0dcce5ef0a8d869ca87d2e112f3a286868555d
GabTorres17 <g.abotorres@hotmail.com>
2024-10-13 12:44:30 -0400

    fix(reviews): rearranged properties by alphabetical order

--- src/app/home/components/Reviews.tsx
+++ src/app/home/components/Reviews.tsx
@@ -64,36 +64,36 @@ export const Reviews: React.FC = () => {
 
   return (
     <div
-      className={`flex flex-col gap-20 ${fullReviews ? 'bg-gray-200' : ''} ${fullReviews && isMobile ? 'bg-black-1' : ''} `}
+      className={`flex flex-col gap-20 ${fullReviews ? 'bg-gray-200' : ''} ${fullReviews && isMobile ? 'bg-black-1' : ''}`}
     >
       <span className='text-center font-darker-grotesque text-[20px] font-extrabold text-blue-6 sm:text-lg'>
         ¿Qué opina nuestra comunidad?
       </span>
       <div
-        className={`flex flex-wrap items-center justify-center ${!isMobile ? 'flex flex-row flex-wrap' : ''} `}
+        className={`flex flex-wrap items-center justify-center ${!isMobile ? 'flex flex-row flex-wrap' : ''}`}
       >
         <ChevronLeft
           onClick={prevReviews}
-          className={`ml-5 h-10 w-10 text-red-400 ${fullReviews && !isMobile ? 'translate-y-[-48px]' : ''} `}
+          className={`ml-5 h-10 w-10 text-red-400 ${fullReviews && !isMobile ? 'translate-y-[-48px]' : ''}`}
         />
         <div
-          className={`flex flex-wrap place-content-center gap-12 ${fullReviews ? 'flex flex-wrap gap-5' : ''}`}
+          className={`flex flex-wrap gap-12 place-content-center  ${fullReviews ? 'flex flex-wrap gap-5' : ''} `}
         >
           {reviews
             .slice(currentIndex, currentIndex + itemsPerPage)
             .map((review: Review) => (
               <div
+                className={`flex w-[300px] flex-col gap-4 rounded-[5%] bg-black-2 py-8 pl-6 text-blue-600 shadow-lg ${fullReviews && !isMobile ? 'bg-black-1 h-auto gap-5 py-6 pl-8 pr-4 text-blue-100 opacity-50 transition-transform duration-300 hover:scale-[1.3] hover:opacity-100' : 'w-56 opacity-100'} ${hoveredCard === review.id && fullReviews ? 'z-10 opacity-100 transition-transform duration-300' : ''} ${fullReviews && isMobile ? 'h-auto gap-5 py-6 pl-8 pr-4 text-blue-100' : ''} `}
                 key={review.id}
-                className={`flex w-[300px] flex-col gap-4 rounded-[5%] bg-black-2 py-8 pl-6 pr-8 text-blue-600 shadow-lg ${fullReviews && !isMobile ? 'h-auto gap-5 bg-black-1 py-6 pl-8 pr-4 text-blue-100 opacity-50 transition-transform duration-300 hover:scale-[1.3] hover:opacity-100' : 'w-56 opacity-100'} ${hoveredCard === review.id && fullReviews ? 'z-10 opacity-100 transition-transform duration-300' : ''} ${fullReviews && isMobile ? 'h-auto gap-5 py-6 pl-8 pr-4 text-blue-100' : ''} `}
                 onMouseEnter={() => setHoveredCard(review.id)}
                 onMouseLeave={() => setHoveredCard(null)}
               >
                 <div className='flex items-center gap-4'>
                   <div className='flex overflow-hidden rounded-full border-2 border-red-300'>
                     <img
+                      alt={review.name}
                       className='w-16'
                       src={review.profile}
-                      alt={review.name}
                     />
                   </div>
                   <div>
@@ -105,24 +105,24 @@ export const Reviews: React.FC = () => {
                         {review.position}
                       </div>
                       <img
+                        alt='Nacionalidad'
                         className='mt-1 flex w-5'
                         src={review.flag}
-                        alt='Nacionalidad'
                       />
                     </div>
                   </div>
                   {hoveredCard === review.id && (
                     <button
-                      onClick={toggleReadMore}
                       className='ml-auto flex self-start rounded-none bg-red-400 bg-transparent font-darker-grotesque text-7 font-extrabold'
+                      onClick={toggleReadMore}
                     >
                       {fullReviews ? (
                         <img
+                          alt='Cerrar'
+                          className='w-6'
                           src={
                             'https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/Reviews%20icons%2Fx.svg?alt=media&token=3fca622b-ce3c-436b-9372-0208bf399ba4'
                           }
-                          alt='Cerrar'
-                          className='w-6'
                         />
                       ) : (
                         ''
@@ -144,8 +144,8 @@ export const Reviews: React.FC = () => {
                 </div>
                 {!fullReviews && (
                   <button
-                    onClick={toggleReadMore}
                     className='mt-auto flex justify-end self-end border-none font-darker-grotesque text-xs font-medium text-red-400'
+                    onClick={toggleReadMore}
                   >
                     Seguir leyendo
                   </button>
@@ -154,15 +154,15 @@ export const Reviews: React.FC = () => {
             ))}
         </div>
         <ChevronRight
-          onClick={nextReviews}
           className={`ml-5 h-10 w-10 text-red-400 ${fullReviews && !isMobile ? 'translate-y-12' : ''} `}
+          onClick={nextReviews}
         />
       </div>
       <Pagination
         currentIndex={currentIndex}
         itemsPerPage={itemsPerPage}
-        totalItems={reviews.length}
         onPageChange={handlePageChange}
+        totalItems={reviews.length}
       />
     </div>
   )
