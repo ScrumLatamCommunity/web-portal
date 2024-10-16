9e65a1f9ceb4953d141b11e28cc647dfa9716f66
David-Rodriguez-1 <ezequiel-07@hotmail.com>
2024-10-15 21:23:30 -0300

    refactor: code refactor in PrimaryButton component

--- src/core/PrimaryButton.tsx
+++ src/core/PrimaryButton.tsx
@@ -2,14 +2,8 @@ interface PrimaryButtonProps {
   label: string
 }
 
-export const PrimaryButton: React.FC<PrimaryButtonProps> = ({ label }) => {
-  return (
-    <>
-      <button className='bg-red-400 rounded-[20px] h-[49px] w-[185px] font-darker-grotesque'>
-        <span className='text-black-1 font-darker-grotesque-3 text-6'>
-          {label}
-        </span>
-      </button>
-    </>
-  )
-}
+export const PrimaryButton: React.FC<PrimaryButtonProps> = ({ label }) => (
+  <button className='bg-red-400 rounded-[20px] h-[49px] w-[185px] font-darker-grotesque'>
+    <span className='text-black-1 font-darker-grotesque-3 text-6'>{label}</span>
+  </button>
+)
