427233105d5e43333da08c2699f395079cf3abe0
David-Rodriguez-1 <ezequiel-07@hotmail.com>
2024-10-15 21:40:53 -0300

    feat: add component

--- src/app/home/components/index.ts
+++ src/app/home/components/index.ts
@@ -2,5 +2,6 @@ import { WelcomeToCommunity } from './WelcomeToCommunity'
 import { Footer } from './Footer'
 import { Navbar } from './Navbar'
 import { Reviews } from './Reviews'
+import { Flags } from './Flags'
 
-export { Navbar, WelcomeToCommunity, Footer, Reviews }
+export { Navbar, WelcomeToCommunity, Footer, Reviews, Flags }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { WelcomeToCommunity, Reviews } from './home/components/index'
+import { WelcomeToCommunity, Reviews, Flags } from './home/components/index'
 import { darkerGrotesque, karla, roboto } from '@/fonts'
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
       className={`${darkerGrotesque.variable} ${karla.variable} ${roboto.variable}`}
     >
       <WelcomeToCommunity />
+      <Flags />
       <Reviews />
     </main>
   )
