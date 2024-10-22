9d1595aba856b27d97daf453c92990952d047746
David-Rodriguez-1 <ezequiel-07@hotmail.com>
2024-10-22 17:34:25 -0300

    fix: welcomeToCommunity component style correction

--- src/core/PrimaryButton.tsx
+++ src/core/PrimaryButton.tsx
@@ -3,7 +3,7 @@ interface PrimaryButtonProps {
 }
 
 export const PrimaryButton: React.FC<PrimaryButtonProps> = ({ label }) => (
-  <button className='h-[49px] w-[185px] rounded-[20px] bg-red-400 font-darker-grotesque'>
+  <button className='rounded-[20px] bg-red-400 px-5 py-[.5vw] font-darker-grotesque'>
     <span className='text-6 font-darker-grotesque-3 text-black-1'>{label}</span>
   </button>
 )
