9cd00b4d22b070d13fdcfbcb52d89b089f6fef74
aljodavid10 <aljodavid100@gmail.com>
2024-10-25 11:26:13 -0500

    feat: fix mobile version

--- src/app/home/components/Navbar.tsx
+++ src/app/home/components/Navbar.tsx
@@ -47,42 +47,44 @@ export const Navbar: React.FC = () => {
     return (
       <>
         <a
-          className='flex flex-grow items-center lg:justify-center md:item-start gap-2 py-2 pr-4 font-medium text-blue-7 hover:text-red-500 hover:underline'
+          className='border-b-2 border-gray-200 lg:border-b-0 flex flex-grow items-center lg:justify-center md:item-start gap-2 py-2 pr-4 font-medium text-blue-7 hover:text-red-500 hover:underline'
           href='#'
         >
           Inicio
         </a>
+
         <a
-          className='flex-grow '
-          href='#'
+          className='border-b-2 border-gray-200 lg:border-b-0 flex-grow '
           onClick={() => toggleMenu('comunidad')}
         >
-          <div className='flex cursor-pointer items-center gap-2 py-2 pr-4 font-medium text-blue-7 md:justify-between sm:justify-between hover:text-red-500 hover:underline'>
+          <div
+            className={`flex cursor-pointer items-center gap-2 py-2 pr-4 font-medium text-blue-7 ${screen === 'sm' || screen === 'md' ? 'justify-between' : 'justify-center'} hover:text-red-500 hover:underline`}
+          >
             Comunidad
             {screen === 'lg' || screen === 'xl' ? (
               <ChevronDown
-                className={`h-3 w-3 transition-transform lg:block ${
+                className={`h-5 w-5 transition-transform lg:block ${
                   activeMenu === 'comunidad' ? 'rotate-180' : ''
                 }`}
                 strokeWidth={2.5}
               />
             ) : activeMenu === 'comunidad' ? (
               <ChevronDown
-                className='h-3 w-3 transition-transform lg:block'
+                className='h-6 w-6 transition-transform lg:block' // Aumenta el tamaño para mobile
                 strokeWidth={2.5}
               />
             ) : (
               <ChevronRight
-                className='h-3 w-3 transition-transform lg:block'
+                className='h-6 w-6 transition-transform lg:block' // Aumenta el tamaño para mobile
                 strokeWidth={2.5}
               />
             )}
           </div>
           {activeMenu === 'comunidad' && (
-            <div className='lg:absolute lg:mt-4 lg:w-64 lg:bg-black-2 md:bg-black-3 lg:p-2 lg:shadow-lg z-10'>
+            <div className='lg:absolute lg:mt-4 lg:p-6 lg:w-80 lg:bg-black-2 md:bg-black-3 md:text-5 lg:shadow-lg z-10'>
               <ul className='space-y-4'>
                 <li className='space-y-4'>
-                  <h3 className='font-bold'>Historia</h3>
+                  <h3>Historia</h3>
                   <div className='flex items-start'>
                     <img
                       alt='Opción 1'
@@ -90,7 +92,7 @@ export const Navbar: React.FC = () => {
                       src='https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/navbar%2Femoji_pencil_orange.svg?alt=media&token=f1261db1-c7d1-407e-8c41-153761031e0f'
                     />
                     <div className='ml-3'>
-                      <p className='font-bold'>Nuestros inicios</p>
+                      <p className='lg:font-bold'>Nuestros inicios</p>
                       <p className='text-sm text-gray-500'>
                         Descubre como se fundo nuestra comunidad y como empezo
                         el camino de Scrum Latam
@@ -99,7 +101,7 @@ export const Navbar: React.FC = () => {
                   </div>
                 </li>
                 <li className='space-y-4'>
-                  <h3 className='font-bold'>¿Quienes somos?</h3>
+                  <h3>¿Quienes somos?</h3>
                   <div className='flex items-start'>
                     <img
                       alt='Opción 1'
@@ -107,7 +109,7 @@ export const Navbar: React.FC = () => {
                       src='https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/navbar%2Femoji_user_blue.svg?alt=media&token=d3a4b3b8-49e0-40f0-a71d-a00f5224f46e'
                     />
                     <div className='ml-3'>
-                      <p className='font-bold'>Los Squads</p>
+                      <p className='lg:font-bold'>Los Squads</p>
                       <p className='text-sm text-gray-500'>
                         Conoce a todos nuestros squads, los roles de cada uno y
                         que rol cumplen dentro de la comunidad
@@ -119,38 +121,37 @@ export const Navbar: React.FC = () => {
             </div>
           )}
         </a>
-        <div className='relative flex-grow'>
+        <div className='border-b-2 border-gray-200 lg:border-b-0 relative flex-grow'>
           <a
             className='flex cursor-pointer items-center lg:justify-center md:item-start gap-2 py-2 pr-4 font-medium text-blue-7 md:justify-between sm:justify-between  hover:text-red-500 hover:underline'
-            href='#'
             onClick={() => toggleMenu('entrenamiento')}
           >
             Entrenamiento
             {screen === 'lg' || screen === 'xl' ? (
               <ChevronDown
-                className={`h-3 w-3 transition-transform lg:block ${
+                className={`h-5 w-5 transition-transform lg:block ${
                   activeMenu === 'entrenamiento' ? 'rotate-180' : ''
                 }`}
                 strokeWidth={2.5}
               />
             ) : activeMenu === 'entrenamiento' ? (
               <ChevronDown
-                className='h-3 w-3 transition-transform lg:block'
+                className='h-6 w-6 transition-transform lg:block' // Aumenta el tamaño para mobile
                 strokeWidth={2.5}
               />
             ) : (
               <ChevronRight
-                className='h-3 w-3 transition-transform lg:block'
+                className='h-6 w-6 transition-transform lg:block' // Aumenta el tamaño para mobile
                 strokeWidth={2.5}
               />
             )}
           </a>
 
           {activeMenu === 'entrenamiento' && (
-            <div className='lg:absolute lg:mt-4 lg:w-64 lg:bg-black-2 md:bg-black-3 lg:p-2 lg:shadow-lg z-10'>
+            <div className='w-full lg:absolute lg:mt-4 lg:p-6 lg:w-80 lg:bg-black-2 md:bg-black-3 lg:shadow-lg z-10'>
               <ul className='space-y-4'>
                 <li className='space-y-4'>
-                  <h3 className='font-bold'>Eventos</h3>
+                  <h3>Eventos</h3>
                   <div className='flex items-start'>
                     <img
                       alt='Opción 1'
@@ -158,7 +159,7 @@ export const Navbar: React.FC = () => {
                       src='https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/navbar%2FIcono%20cara.svg?alt=media&token=07a8e8e3-7c4c-416f-9462-9d116a5d2e99'
                     />
                     <div className='ml-3'>
-                      <p className='font-bold'>Lorem ipsum dolor</p>
+                      <p className='lg:font-bold'>Lorem ipsum dolor</p>
                       <p className='text-sm text-gray-500'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Donec vel egestas dolor, nec dignissim metus.
@@ -167,7 +168,7 @@ export const Navbar: React.FC = () => {
                   </div>
                 </li>
                 <li className='space-y-4'>
-                  <h3 className='font-bold'>Webinars</h3>
+                  <h3>Webinars</h3>
                   <div className='flex items-start'>
                     <img
                       alt='Opción 1'
@@ -175,7 +176,7 @@ export const Navbar: React.FC = () => {
                       src='https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/navbar%2FGroup%20633047.svg?alt=media&token=ce02d28a-dfdd-4f4c-8e89-8944651d3a12'
                     />
                     <div className='ml-3'>
-                      <p className='font-bold'>Lorem ipsum dolor</p>
+                      <p className='lg:font-bold'>Lorem ipsum dolor</p>
                       <p className='text-sm text-gray-500'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Donec vel egestas dolor, nec dignissim metus.
@@ -184,7 +185,7 @@ export const Navbar: React.FC = () => {
                   </div>
                 </li>
                 <li className='space-y-4'>
-                  <h3 className='font-bold'>Talleres</h3>
+                  <h3>Talleres</h3>
                   <div className='flex items-start'>
                     <img
                       alt='Opción 1'
@@ -192,7 +193,7 @@ export const Navbar: React.FC = () => {
                       src='https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/navbar%2FIcono%20libro.svg?alt=media&token=e668d076-74d4-4440-b444-938ab4ca08c4'
                     />
                     <div className='ml-3'>
-                      <p className='font-bold'>Lorem ipsum dolor</p>
+                      <p className='lg:font-bold'>Lorem ipsum dolor</p>
                       <p className='text-sm text-gray-500'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Donec vel egestas dolor, nec dignissim metus.
@@ -206,14 +207,14 @@ export const Navbar: React.FC = () => {
         </div>
 
         <a
-          className='flex flex-grow items-center lg:justify-center md:item-start gap-2 py-2 pr-4 font-medium text-blue-7 hover:text-red-500 hover:underline'
+          className='border-b-2 border-gray-200 lg:border-b-0 flex flex-grow items-center lg:justify-center md:item-start gap-2 py-2 pr-4 font-medium text-blue-7 hover:text-red-500 hover:underline'
           href='#'
         >
           Documentación
         </a>
 
         <a
-          className='flex flex-grow items-center lg:justify-center md:item-start gap-2 py-2 pr-4 font-medium text-blue-7 hover:text-red-500 hover:underline'
+          className='border-b-2 border-gray-200 lg:border-b-0 flex flex-grow items-center lg:justify-center md:item-start gap-2 py-2 pr-4 font-medium text-blue-7 hover:text-red-500 hover:underline'
           href='#'
         >
           Novedades
@@ -223,13 +224,13 @@ export const Navbar: React.FC = () => {
   }
 
   return (
-    <header className='bg-black-3 border-b-2 border-gray-200 font-DM'>
-      <div className='container mx-auto py-4 px-4 lg:flex lg:items-center lg:justify-between'>
-        <div className='scroll flex items-center justify-between lg:justify-start'>
-          <a href='#' className='overflow-visible'>
+    <header className='sticky top-0 z-50 bg-black-3 border-b-2 border-gray-200 font-DM'>
+      <div className='mx-auto  py-4 px-4 lg:flex lg:items-center lg:justify-between'>
+        <div className='scroll flex lg:justify-start justify-between'>
+          <a href='#' className='flex flex-row justify-start'>
             <img
               alt='logo'
-              className='h-7 px-1'
+              className='h-12 w-auto' // Aumenta la altura, el ancho es automático para mantener la proporción.
               src='https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/navbar%2FScrum%20logo%20principal.svg?alt=media&token=d8cce1e3-c821-4e52-9596-289f17c63203'
             />
           </a>
@@ -237,7 +238,7 @@ export const Navbar: React.FC = () => {
           {/* Enlace de navegación visible en pantallas pequeñas */}
           {(screen === 'sm' || screen === 'md') && (
             <div className='flex flex-row items-center mt-4'>
-              <div className='space-x-6 flex items-center'>
+              <div className='space-x-6 flex items-center whitespace-nowrap'>
                 <a
                   className='block text-blue-7 hover:text-red-400 p-2 flex items-center'
                   href='#'
@@ -264,27 +265,25 @@ export const Navbar: React.FC = () => {
         </div>
 
         {/* Enlace de navegación visible en pantallas medianas y grandes */}
-        <nav className='hidden lg:flex overflow-auto space-x-7 z-10'>
-          {navList()}
-        </nav>
+        <nav className='hidden lg:flex z-10'>{navList()}</nav>
 
         {/* Menú de navegación para móviles */}
         <div
           className={`${
             openNav ? '' : 'hidden'
-          } mt-4 bg-black-3 flex flex-col gap-4 p-6 rounded lg:hidden`}
+          } mt-4 bg-black-3 flex flex-col gap-4 p-6 rounded lg:hidden text-7 w-screen h-max`}
         >
           {navList()}
         </div>
         {/* Contenedor de botones en pantallas medianas y grandes */}
         {(screen === 'lg' || screen === 'xl') && (
           <div className='hidden lg:flex flex-row items-center mt-2'>
-            <div className='space-x-6 flex items-center'>
+            <div className='flex items-center'>
               <a
                 className='text-blue-7 hover:text-red-400 p-2 flex items-center'
                 href='#'
               >
-                <User className='h-4 mr-2' />
+                <User className='h-4' />
                 <h2 className='block'>Iniciar sesión</h2>
               </a>
               <a
