fe9dbd15ac00e53f080c8679b4c6159180e35556
Jorge Martínez <jorgemartinez.jam@gmail.com>
2024-11-23 17:35:25 -0300

    fix: location and sizes of text and images, desktop and mobile version (#37)

--- src/app/home/components/WelcomeToCommunity.tsx
+++ src/app/home/components/WelcomeToCommunity.tsx
@@ -22,7 +22,7 @@ export const WelcomeToCommunity = () => {
             ¡Bienvenido!
           </h1>
           <h2 className='text-12 font-extrabold leading-8 opacity-80 md:w-11/12 md:text-11 md:leading-[30px] xl:w-11/12 xl:text-17 xl:leading-1'>
-            a Scrum Latam: Donde la Comunidad y la Agilidad Convergen
+            Scrum Latam: Donde la Comunidad y la Agilidad Convergen
           </h2>
           <p className='w-[86%] pt-6 font-karla text-5 font-semibold leading-[21.6px] text-blue-8 md:w-[100%] md:text-4 md:leading-[23.23px] xl:w-[85.1%] xl:text-10 xl:leading-8'>
             Nos dedicamos al aprendizaje colaborativo y a la implementación de
