110df0435b77db29d4e516e900c19cc8b1232a70
GabTorres17 <g.abotorres@hotmail.com>
2024-12-03 13:56:33 -0400

    chore(modal): changed modal styling as requested

--- src/core/Modal.tsx
+++ src/core/Modal.tsx
@@ -40,22 +40,22 @@ export const Modal: React.FC<ModalProps> = ({
         className='fixed inset-0 z-50 flex items-center justify-center bg-black-10 bg-opacity-50'
         onClick={handleBackdropClick}
       >
-        <div className='relative flex max-h-[85vh] w-full justify-start overflow-y-auto rounded-lg bg-white shadow-lg scrollbar-thin scrollbar-thumb-black-8 scrollbar-thumb-rounded-full sm:w-3/5 sm:scrollbar'>
-          <div className='sm:10 flex w-full flex-col items-start gap-3 p-8 md:px-24'>
+        <div className='relative flex max-h-[85vh] w-full justify-start overflow-y-auto rounded-lg bg-white shadow-lg scrollbar-thin scrollbar-thumb-black-8 scrollbar-thumb-rounded-full sm:w-4/5 lg:w-3/5'>
+          <div className='flex w-full flex-col items-start gap-3 p-8 md:px-24'>
             {title && (
               <h2 className='flex font-karla text-3xl font-bold text-red-500'>
                 {title}
               </h2>
             )}
-            <div className='pr-7'>{children}</div>
+            <div className='pb-10 pr-7'>{children}</div>
           </div>
           <button
-            className='fixed right-[15%] top-20 mb-8 flex w-auto sm:right-[22%] sm:h-6'
+            className='fixed right-[8%] top-20 mb-8 flex w-auto sm:right-[15%] sm:h-6 lg:right-[22%]'
             onClick={onClose}
           >
             <img
               alt='cerrar'
-              className='h-6 w-6 gap-1 sm:h-[50px] sm:w-[50px]'
+              className='h-6 w-6 gap-1 md:h-[50px] md:w-[50px]'
               src='https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/Reviews%20icons%2Fx.svg?alt=media&token=3fca622b-ce3c-436b-9372-0208bf399ba4'
             />
           </button>
