9a568391bff1f2f9c2cfdf9527e3f449129a6ec4
Jorge Martínez <jorgemartinez.jam@gmail.com>
2024-12-05 12:35:03 -0300

    feat: new activities view and menu features for redirection (#43)
    * feat: new activities view and menu features for redirection
    
    * fix: responsive view for activities
    
    * fix: responsive view for activities
    
    * fix: alphabetical order
    
    * fix: styles and alphabetical order
    
    * fix: responsive view rigth padding
    
    * fix: typography and positions squads and activities herosection
    
    * fix: margin cards

---
+++ src/app/activities/interfaces/workshopInterface.tsx
@@ -0,0 +1,12 @@
+export interface Workshop {
+  id: number
+  image: string // Cambiar a string si siempre es una URL
+  title: string
+  leader: string
+  schedule: string
+  description: string
+  link: string
+  country: string
+  flag: string // Cambiar a string si siempre es una URL
+  details: string
+}
