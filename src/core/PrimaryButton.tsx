1ef54b333a181ab7ce772da2834466b4986b4ade
GabTorres17 <g.abotorres@hotmail.com>
2024-12-06 17:39:52 -0400

    fix(joinus): add link to benefits and changed sizes for desktop view

--- src/core/PrimaryButton.tsx
+++ src/core/PrimaryButton.tsx
@@ -1,14 +1,17 @@
 interface PrimaryButtonProps {
   label: string
   className?: string
+  onClick?: () => void
 }
 
 export const PrimaryButton: React.FC<PrimaryButtonProps> = ({
   label,
   className,
+  onClick,
 }) => (
   <button
     className={`rounded-[20px] bg-red-400 px-5 py-[.5vw] font-darker-grotesque ${className}`}
+    onClick={onClick}
   >
     <span className='text-6 font-darker-grotesque-3 text-black-1'>{label}</span>
   </button>
