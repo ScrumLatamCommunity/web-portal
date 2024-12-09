d3c42288a0e799762c0a41e350b225fda5bc4d78
Fernando Campellone <tomascampellone@gmail.com>
2024-12-09 15:57:50 -0300

    fix: added new fonts and fixed some width problems in mobile

--- src/app/memberships/components/Plans.tsx
+++ src/app/memberships/components/Plans.tsx
@@ -3,6 +3,7 @@ import { CheckBox } from '@mui/icons-material'
 import Image from 'next/image'
 import React from 'react'
 import crown from '../imgs/crown.png'
+import { darkerGrotesque, karla, inter } from '@/fonts'
 
 interface PlansProps {
   title: string
@@ -20,68 +21,74 @@ export default function Plans({
   description3,
 }: PlansProps) {
   return (
-    <div className='items-left mx-10 flex h-[553.03px] w-[340px] flex-col rounded-lg bg-gradient-to-b from-[#FD3600] to-[#FFFFFF] p-[2px] shadow-[0px_8px_15px_rgba(0,0,0,0.2)] md:mb-10 md:w-[380px]'>
-      <div className='h-full rounded-[0.4rem] bg-white pl-12 pt-6'>
-        <h1 className='mb-2 flex items-center text-[32px] font-darker-grotesque-4'>
-          {title}
-          {title.toLowerCase() === 'premium' && (
-            <Image
-              alt='Crown Icon'
-              className='ml-2'
-              height={24}
-              src={crown}
-              width={24}
+    <main
+      className={`${darkerGrotesque.variable} ${karla.variable} ${inter.variable}`}
+    >
+      <div className='flex h-[553.03px] w-[336px] flex-col items-center rounded-lg bg-gradient-to-b from-[#FD3600] to-[#FFFFFF] p-[2px] shadow-[0px_8px_15px_rgba(0,0,0,0.2)] md:mx-10 md:mb-10 md:w-[350px]'>
+        <div className='h-full w-full rounded-[0.4rem] bg-white px-6 py-6 md:pl-12 md:pt-6'>
+          <h1 className='mb-2 flex items-center text-[32px] font-darker-grotesque-700'>
+            {title}
+            {title.toLowerCase() === 'premium' && (
+              <Image
+                alt='Crown Icon'
+                className='ml-2'
+                height={24}
+                src={crown}
+                width={24}
+              />
+            )}
+          </h1>
+          <h1 className='mb-4 text-[50px] font-darker-grotesque-500'>
+            {price}
+          </h1>
+          <button className='mb-6 h-[43px] w-[183px] rounded bg-[#FE5833] px-4 py-2 text-[18px] font-inter-500 text-white'>
+            Empezar
+          </button>
+          <div className='w-12/12 my-4 border-t-2 border-gray-300'></div>
+          <div className='mb-10 mt-6 flex flex-row'>
+            <CheckBox
+              sx={{
+                color: '#B299FF',
+                transform: 'scale(1.3)',
+                '& .MuiSvgIcon-root': {
+                  fontSize: '0.8rem',
+                },
+              }}
             />
-          )}
-        </h1>
-        <h1 className='mb-4 text-[50px] font-darker-grotesque-1'>{price}</h1>
-        <button className='mb-6 h-[43px] w-[183px] rounded bg-[#FE5833] px-4 py-2 text-[18px] text-white'>
-          Empezar
-        </button>
-        <div className='my-2 w-10/12 border-t-2 border-gray-300'></div>
-        <div className='mb-10 mt-6 flex flex-row'>
-          <CheckBox
-            sx={{
-              color: '#B299FF',
-              transform: 'scale(1.3)',
-              '& .MuiSvgIcon-root': {
-                fontSize: '0.8rem',
-              },
-            }}
-          />
-          <p className='ml-2 pl-4 text-[18px] font-darker-grotesque-1'>
-            {description1}
-          </p>
-        </div>
-        <div className='mb-10 mt-6 flex flex-row'>
-          <CheckBox
-            sx={{
-              color: '#B299FF',
-              transform: 'scale(1.3)',
-              '& .MuiSvgIcon-root': {
-                fontSize: '0.8rem',
-              },
-            }}
-          />
-          <p className='ml-2 pl-4 text-[18px] font-darker-grotesque-1'>
-            {description2}
-          </p>
-        </div>
-        <div className='mb-10 mt-6 flex flex-row'>
-          <CheckBox
-            sx={{
-              color: '#B299FF',
-              transform: 'scale(1.3)',
-              '& .MuiSvgIcon-root': {
-                fontSize: '0.8rem',
-              },
-            }}
-          />
-          <p className='ml-2 pl-4 text-[18px] font-darker-grotesque-1'>
-            {description3}
-          </p>
+            <p className='ml-2 pl-4 text-[18px] font-darker-grotesque-600'>
+              {description1}
+            </p>
+          </div>
+          <div className='mb-10 mt-6 flex flex-row'>
+            <CheckBox
+              sx={{
+                color: '#B299FF',
+                transform: 'scale(1.3)',
+                '& .MuiSvgIcon-root': {
+                  fontSize: '0.8rem',
+                },
+              }}
+            />
+            <p className='ml-2 pl-4 text-[18px] font-darker-grotesque-600'>
+              {description2}
+            </p>
+          </div>
+          <div className='mb-10 mt-6 flex flex-row'>
+            <CheckBox
+              sx={{
+                color: '#B299FF',
+                transform: 'scale(1.3)',
+                '& .MuiSvgIcon-root': {
+                  fontSize: '0.8rem',
+                },
+              }}
+            />
+            <p className='ml-2 pl-4 text-[18px] font-darker-grotesque-600'>
+              {description3}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   )
 }
--- src/app/memberships/page.tsx
+++ src/app/memberships/page.tsx
@@ -7,22 +7,27 @@ import chat from './imgs/chat.png'
 import sobre from './imgs/sobre.png'
 import ideas from './imgs/ideas.png'
 import meeting from './imgs/meeting.png'
+import { darkerGrotesque, karla } from '@/fonts'
 import { memberships } from '@/data/data'
 import { benefits } from '@/data/data'
 
 export default function Memberships() {
   return (
-    <>
-      <section className='flex flex-col items-center pt-12 md:pt-32'>
-        <h1 className='pb-3 text-center text-16 text-3xl font-darker-grotesque-4 md:pb-6 md:text-4xl lg:text-5xl'>
+    <main className={`${darkerGrotesque.variable} ${karla.variable}`}>
+      <section className='flex flex-col items-center pt-12 font-darker-grotesque md:pt-32'>
+        <h1
+          className={`pb-3 text-center text-[30px] font-darker-grotesque-700 md:mr-0 md:pb-6 md:text-[48px]`}
+        >
           Nuestras Membresías
         </h1>
         <div className='absolute right-6 top-40 -z-[1] hidden h-[40dvw] w-[50dvw] rounded-full bg-red-300 opacity-20 blur-3xl md:block'></div>
-        <h2 className='font-darker-grotesque-2 mx-auto mb-12 h-[41px] w-[290px] text-center text-3 md:mb-40 md:h-auto md:w-auto md:text-3xl'>
+        <h2
+          className={`mb-12 h-[41px] w-[290px] text-center text-3 font-darker-grotesque-600 md:mb-40 md:mr-0 md:h-auto md:w-auto md:text-3xl`}
+        >
           Únete a una comunidad especial y goza de multiples beneficios
         </h2>
-        <div className='absolute -bottom-[300px] left-8 -z-[1] hidden h-[30dvw] w-[20dvw] rounded-full bg-red-300 opacity-20 blur-3xl md:block'></div>
-        <div className='flex flex-col justify-center gap-6 md:flex-row md:flex-wrap'>
+        <div className='absolute -z-[1] hidden rounded-full bg-red-300 opacity-100 blur-3xl md:-bottom-[300px] md:left-8 md:block md:h-[30dvw] md:w-[20dvw]'></div>
+        <div className='flex flex-col items-center gap-6 md:flex-row md:flex-wrap'>
           {memberships.map((membership, index) => {
             const isLeft = index === 0
             const isMiddle = index === Math.floor(memberships.length / 2)
@@ -58,31 +63,31 @@ export default function Memberships() {
               className='h-[87px] w-[87px] md:mb-2 md:mt-8 md:h-[169px] md:w-[169px]'
               src={ideas}
             />
-            <p className='font-darker-grotesque-2 w-[184px] text-[12px] md:w-[310px] md:text-[18px]'>
+            <p className='w-[184px] text-[12px] font-darker-grotesque-600 md:w-[310px] md:text-[18px]'>
               Contribuye en proyectos colaborativos y ayúdanos a crecer, ya sea
               facilitando sesiones o moderando foros, lo cual además potenciará
               tu perfil de liderazgo.
             </p>
           </div>
-          <div className='flex-fil font-darker-grotesque-2 mb-12 flex items-center gap-6 md:mx-20 md:flex-col'>
+          <div className='flex-fil mb-12 flex items-center gap-6 md:mx-20 md:flex-col'>
             <Image
               alt='sobre'
               className='h-[87px] w-[87px] md:h-[169px] md:w-[169px]'
               src={sobre}
             />
-            <p className='w-[184px] text-[12px] md:w-[310px] md:text-[18px]'>
+            <p className='w-[184px] text-[12px] font-darker-grotesque-600 md:w-[310px] md:text-[18px]'>
               Obtén un distintivo digital que demuestra tu pertenencia a la
               comunidad, el cual puedes compartir en redes profesionales como
               LinkedIn.
             </p>
           </div>
-          <div className='flex-fil font-darker-grotesque-2 mb-12 flex items-center gap-6 md:mx-20 md:flex-col'>
+          <div className='flex-fil mb-12 flex items-center gap-6 md:mx-20 md:flex-col'>
             <Image
               alt='chat'
               className='h-[87px] w-[87px] md:h-[169px] md:w-[169px]'
               src={chat}
             />
-            <p className='w-[184px] text-[12px] md:w-[310px] md:text-[18px]'>
+            <p className='w-[184px] text-[12px] font-darker-grotesque-600 md:w-[310px] md:text-[18px]'>
               Asiste a reuniones virtuales, foros y sesiones de preguntas y
               respuestas donde podrás interactuar con expertos y otros miembros
               para resolver dudas.
@@ -90,22 +95,29 @@ export default function Memberships() {
           </div>
         </div>
       </section>
-      <section className='flex-fil flex'>
+      <section
+        className={`flex-fil flex ${darkerGrotesque.variable} ${karla.variable} `}
+      >
         <div className='absolute left-0 top-[2300px] -z-10 mt-72 flex h-[505.44px] w-screen skew-y-[-4.52deg] flex-col items-center bg-gradient-to-b from-[#FE2E00] to-[#FCFCFC] pt-44 opacity-80 md:top-[1800px]'></div>
         <div className='flex flex-col items-center md:flex-row'>
           <div className='flex-1 md:order-last md:mr-10'>
-            <h1 className='mx-auto mt-40 w-full max-w-[283px] pb-6 text-center text-[32px] font-darker-grotesque-4 md:ml-0 md:mt-96 md:max-w-[620.02px] md:text-[48px]'>
-              ¿Por qué unirse a nosotros?
-            </h1>
-            <p className='font-darker-grotesque-2 mx-auto w-[330px] text-center text-[16px] md:ml-0 md:w-[623px] md:max-w-[623px] md:text-[29px]'>
-              Únete a Scrum Latam y transforma tu carrera junto a una comunidad
-              que impulsa el cambio ágil en toda Latinoamérica. Aquí no solo
-              aprendes frameworks; creces como líder, inspiras a otros, y eres
-              parte de un movimiento que está revolucionando el trabajo en
-              equipo y la entrega de valor. ¡Ven y marca la diferencia, donde
-              cada interacción es una oportunidad para innovar y cada
-              colaboración fortalece la agilidad de toda una región!
-            </p>
+            <div className='font-darker-grotesque'>
+              <h1 className='mx-auto mt-40 w-full max-w-[283px] pb-6 text-center text-[32px] font-darker-grotesque-700 md:ml-0 md:mt-96 md:max-w-[620.02px] md:text-[48px]'>
+                ¿Por qué unirse a nosotros?
+              </h1>
+            </div>
+            <div className='font-karla'>
+              <p className='mx-auto w-[330px] text-center text-[16px] font-karla-400 md:ml-0 md:w-[599px] md:max-w-[623px] md:text-[27px]'>
+                Únete a Scrum Latam y transforma tu carrera junto a una
+                comunidad que impulsa el cambio ágil en toda Latinoamérica. Aquí
+                no solo aprendes frameworks; creces como líder, inspiras a
+                otros, y eres parte de un movimiento que está revolucionando el
+                trabajo en equipo y la entrega de valor. ¡Ven y marca la
+                diferencia, donde cada interacción es una oportunidad para
+                innovar y cada colaboración fortalece la agilidad de toda una
+                región!
+              </p>
+            </div>
           </div>
           <div className='md:order-first md:ml-10 md:mr-40'>
             <Image
@@ -116,33 +128,37 @@ export default function Memberships() {
           </div>
         </div>
       </section>
-      <section className='mt-44 flex flex-col items-center'>
-        <h1 className='mx-auto mb-10 w-full max-w-[300px] pb-6 text-center text-[24px] font-darker-grotesque-4 md:max-w-[620.02px] md:text-[48px]'>
-          Nuestros Beneficios
-        </h1>
-        <div className='flex-fil mb-1 flex items-center gap-5 md:self-end'>
-          <p className='ml-40 text-[14px] font-darker-grotesque-4 md:text-[29px]'>
-            Gratuito
-          </p>
-          <p className='text-[14px] font-darker-grotesque-4 md:ml-52 md:text-[29px]'>
-            Flex
-          </p>
-          <p className='text-[14px] font-darker-grotesque-4 md:ml-52 md:mr-16 md:text-[29px]'>
-            Premium
-          </p>
+      <section className='mt-12 flex flex-col items-center md:mt-44'>
+        <div className='font-darker-grotesque'>
+          <h1 className='mx-auto mb-4 w-full max-w-[300px] pb-6 text-center text-[24px] font-darker-grotesque-700 md:mb-10 md:max-w-[620.02px] md:text-[48px]'>
+            Nuestros Beneficios
+          </h1>
         </div>
-        <div className='mb-32 flex flex-col md:mb-56'>
-          {benefits.map((benefit) => (
-            <Benefits
-              key={benefit.id}
-              text={benefit.text}
-              checkFree={benefit.checkFree}
-              checkFlex={benefit.checkFlex}
-              checkPremium={benefit.checkPremium}
-            />
-          ))}
+        <div className='mx-auto items-center'>
+          <div className='flex-fil mb-1 flex items-center gap-5 md:self-end'>
+            <p className='ml-[172px] text-[13px] font-darker-grotesque-600 md:ml-[505px] md:text-[32px] md:font-darker-grotesque-700'>
+              Gratuito
+            </p>
+            <p className='text-[13px] font-darker-grotesque-600 md:ml-[200px] md:text-[32px] md:font-darker-grotesque-700'>
+              Flex
+            </p>
+            <p className='text-[13px] font-darker-grotesque-600 md:ml-[200px] md:mr-2 md:text-[32px] md:font-darker-grotesque-700'>
+              Premium
+            </p>
+          </div>
+          <div className='mb-32 flex flex-col md:mb-56'>
+            {benefits.map((benefit) => (
+              <Benefits
+                key={benefit.id}
+                text={benefit.text}
+                checkFree={benefit.checkFree}
+                checkFlex={benefit.checkFlex}
+                checkPremium={benefit.checkPremium}
+              />
+            ))}
+          </div>
         </div>
       </section>
-    </>
+    </main>
   )
 }
