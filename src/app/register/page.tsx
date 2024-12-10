5a4458446204dc90ead77a38977828df0bddca10
Eric David Diaz <ericdiazeuka@gmail.com>
2024-12-10 01:51:42 -0300

    fix: background circles corrections

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -22,9 +22,9 @@ interface FormData {
 const BackgroundCircles = () => {
   return (
     <div className='absolute inset-0 -z-10'>
-      <div className='absolute left-10 top-40 h-32 w-32 rounded-full bg-gradient-to-r from-[#FD3600] to-[#FF5733] opacity-80 md:left-10 md:top-20'></div>
-      <div className='absolute right-10 top-10 h-32 w-32 rounded-full bg-gradient-to-r from-[#FD3600] to-[#FF5733] opacity-50 md:right-60 md:top-32'></div>
-      <div className='absolute left-40 top-80 h-24 w-24 rounded-full bg-gradient-to-r from-[#FD3600] to-[#FF5733] opacity-30 md:left-40 md:top-60'></div>
+      <div className='absolute left-10 top-40 h-48 w-48 rounded-full bg-gradient-to-r from-[#FD3600] to-[#FF5733] opacity-40 blur-3xl md:left-10 md:top-20'></div>
+      <div className='absolute right-10 top-10 h-48 w-48 rounded-full bg-gradient-to-r from-[#FD3600] to-[#FF5733] opacity-30 blur-3xl md:right-60 md:top-32'></div>
+      <div className='absolute left-40 top-80 h-36 w-36 rounded-full bg-gradient-to-r from-[#FD3600] to-[#FF5733] opacity-20 blur-3xl md:left-40 md:top-60'></div>
     </div>
   )
 }
