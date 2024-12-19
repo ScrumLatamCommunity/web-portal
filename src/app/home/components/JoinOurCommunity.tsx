5d39e34fb24e16a03363ccff0bc9cf2b91b4c626
Gabriel Torres <g.abotorres@hotmail.com>
2024-12-19 16:57:55 -0400

    PWS1-78-JoinUs (#54)
    * feat(history): added first history part of history comp
    
    * feat(history): created reusable modal and Historia
    
    * fix(modal): modified modal behavior for responsive design
    
    * feat(history): history-mision-vision component implemented
    
    * chore(link): added link to /history
    
    * fix(history): alphabet order from mobilemisionvision fixed
    
    * feat(terms): added modal for terms and conditions
    
    * chore(terms): modified modal and terms and conditions
    
    * fix(joinus): add link to benefits and changed sizes for desktop view
    
    * chore(img): changed image as figma design
    
    * fix(footer): alphabetical order

--- src/app/home/components/JoinOurCommunity.tsx
+++ src/app/home/components/JoinOurCommunity.tsx
@@ -2,6 +2,7 @@
 import { PrimaryButton } from '@/core/PrimaryButton'
 import { images } from '@/data/images_url'
 import useIsLargeScreen from '@/hooks'
+import Link from 'next/link'
 
 export const JoinOurCommunity = () => {
   const isLargeScreen = useIsLargeScreen(1300)
@@ -11,35 +12,37 @@ export const JoinOurCommunity = () => {
     : images.joinSectionMobile
 
   return (
-    <div className='relative m-auto flex max-w-screen-2xl flex-col items-center px-12 pb-9'>
+    <div className='relative m-auto flex w-4/6 flex-col items-center justify-center py-0 lg:mb-32'>
       <div className='z-0 flex flex-col items-center overflow-hidden rounded-xl bg-black-4 shadow-xl md:flex-row-reverse'>
-        <div className='flex h-full w-[160%]'>
+        <div className='flex h-full w-1/2'>
           <img
             alt=''
             className='flex h-full'
             height={600}
             src={joinUsImage}
-            width={550}
+            width={600}
           />
         </div>
-        <div className='m-0 flex flex-col items-center gap-[2vw] bg-transparent pb-10 pt-[2rem] md:items-start md:pl-[4rem]'>
-          <h3 className='m-0 text-center font-darker-grotesque text-[6dvw] font-extrabold leading-8 text-blue-6 md:flex md:flex-col md:text-start lg:text-[5dvw]'>
+        <div className='m-0 flex flex-col items-center gap-[2vw] bg-transparent pb-10 pt-[2rem] sm:w-1/2 md:items-start md:pb-0 md:pl-14 md:pt-0 lg:justify-center lg:gap-2 lg:py-8'>
+          <p className='m-0 text-center font-darker-grotesque text-[5dvw] font-extrabold leading-8 text-blue-6 md:flex md:flex-col md:text-start lg:text-15'>
             ¿POR QUÉ UNIRSE
-          </h3>
-          <p className='m-0 text-center font-darker-grotesque text-[6dvw] font-extrabold leading-8 text-red-400 md:flex md:flex-col md:text-start lg:text-[5dvw]'>
+          </p>
+          <p className='m-0 text-center font-darker-grotesque text-[6dvw] font-extrabold leading-8 text-red-400 md:flex md:flex-col md:text-start md:text-17 lg:text-17'>
             A NOSOTROS?
           </p>
-          <p className='w-[80%] text-center font-karla text-[3.5dvw] font-medium leading-3 text-blue-6 md:flex md:text-start md:text-12 md:leading-9 lg:pt-10'>
+          <p className='w-[80%] text-center font-karla text-[3.5dvw] font-medium leading-3 text-blue-6 md:flex md:text-start md:text-7 md:leading-9 lg:pt-10'>
             Conéctate con profesionales ágiles de toda Latinoamérica, accede a
             recursos exclusivos, y participa en eventos y webinars que
             impulsarán tu crecimiento.{' '}
           </p>
-          <p className='w-[80%] text-center font-karla font-bold leading-3 text-blue-6 md:text-start md:text-12 md:leading-9'>
+          <p className='w-[80%] text-center font-karla font-bold leading-3 text-blue-6 md:text-start md:text-7 md:leading-9'>
             ¡Regístrate hoy y sé parte de nuestra transformación ágil!
           </p>
-          <div className='mt-3 flex items-center justify-center'>
-            <PrimaryButton className='w-[160px]' label='Beneficios' />
-          </div>
+          <Link href='/benefits'>
+            <div className='mt-3 flex items-center justify-center'>
+              <PrimaryButton className='w-[160px]' label='Beneficios' />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
