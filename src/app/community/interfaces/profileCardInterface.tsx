c528a1c258e4dba4942cdb2ca6d362253da3169b
David Morcillo <aljodavid100@gmail.com>
2025-04-30 13:40:52 -0500

    Refactor team members data (#111)
    * feat: modify squads profile section
    
    * feat: add flags profile squads

--- src/app/community/interfaces/profileCardInterface.tsx
+++ src/app/community/interfaces/profileCardInterface.tsx
@@ -2,8 +2,7 @@ export interface ProfileCardProps {
   name: string
   title: string
   description: string
-  image: React.ReactNode
-  githubUrl?: string
-  behanceUrl?: string
-  instagramUrl?: string
+  countryFlagUrl: string
+  imageUrl: string
+  linkedinUrl?: string
 }
