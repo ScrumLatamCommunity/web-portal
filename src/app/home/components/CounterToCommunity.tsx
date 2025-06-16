a2314a69d4b5b405aa9fe5bc06b251f3caad3074
Fernando Campellone <tomascampellone@gmail.com>
2025-06-16 13:17:20 -0300

    feat: re-design home

--- src/app/home/components/CounterToCommunity.tsx
+++ src/app/home/components/CounterToCommunity.tsx
@@ -1,42 +1,168 @@
 'use client'
-import { useCounter } from '../hooks/useCounter'
-import { counters } from '@/data/data'
-const gea_icon =
-  'https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/Counters%20icons%2FGea_icon.svg?alt=media&token=6af53d34-e4dd-4db3-9eb8-090c60e8dc95'
+import { FC, SVGProps, useEffect, useState } from 'react'
+import UserIcon from '@/assets/userIcon'
+import StarIcon from '@/assets/starIcon'
+import ActivityIcon from '@/assets/activityIcon'
+import React from 'react'
+import WorldIcon from '@/assets/worldIcon'
+
+interface Counter {
+  id: number
+  number: number
+  text: string
+  icon: FC<SVGProps<SVGSVGElement>>
+}
+
+const initialCounters: Counter[] = [
+  {
+    id: 1,
+    number: 0,
+    text: 'Miembros',
+    icon: UserIcon
+  },
+  {
+    id: 2,
+    number: 0,
+    text: 'Países',
+    icon: WorldIcon
+  },
+  {
+    id: 3,
+    number: 0,
+    text: 'Actividades',
+    icon: ActivityIcon
+  },
+  {
+    id: 4,
+    number: 0,
+    text: 'Sponsors',
+    icon: StarIcon
+  }
+]
 
 export const CounterToCommunity = () => {
-  useCounter(counters)
+  const [currentCounters, setCurrentCounters] =
+    useState<Counter[]>(initialCounters)
+  const [currentActivities, setCurrentActivities] = useState<any[] | undefined>(
+    undefined
+  )
+
+  const getMembers = async () => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}users`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      if (!response.ok) {
+        throw new Error('Error al obtener miembros')
+      }
+
+      const data: { role: string; country: string }[] = await response.json()
+
+      let userCount = 0
+      let sponsorCount = 0
+      const uniqueCountries = new Set<string>()
+
+      data.forEach((user) => {
+        if (user.role === 'USER') {
+          userCount++
+        }
+        if (user.role === 'SPONSOR') {
+          sponsorCount++
+        }
+        if (user.country) {
+          uniqueCountries.add(user.country)
+        }
+      })
+
+      const updatedCounters = initialCounters.map((counter) => {
+        if (counter.text === 'Miembros') {
+          return { ...counter, number: userCount }
+        }
+        if (counter.text === 'Sponsors') {
+          return { ...counter, number: sponsorCount }
+        }
+        if (counter.text === 'Países') {
+          return { ...counter, number: uniqueCountries.size }
+        }
+        return counter
+      })
+
+      setCurrentCounters(updatedCounters)
+    } catch (error) {
+      console.error('Error al obtener miembros:', error)
+    }
+  }
+
+  const getActivities = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}activities`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+
+      if (!response.ok) {
+        throw new Error('Error al obtener actividades')
+      }
+      const data: any[] = await response.json()
+      setCurrentActivities(data)
+
+      setCurrentCounters((prevCounters) =>
+        prevCounters.map((counter) => {
+          if (counter.text === 'Actividades') {
+            return { ...counter, number: data.length }
+          }
+          return counter
+        })
+      )
+    } catch (error) {
+      console.error('Error al obtener actividades:', error)
+    }
+  }
+
+  useEffect(() => {
+    getMembers()
+    getActivities()
+  }, [])
+
   return (
     <section className='relative m-auto mt-8 font-darker-grotesque md:max-w-[1920px] md:pb-16'>
-      <div className='absolute right-44 top-0 z-10 hidden h-[19dvw] w-screen rounded-full bg-red-300 opacity-15 blur-3xl md:block'></div>
-      <div className='absolute bottom-1/4 left-40 z-10 hidden h-[19dvw] w-[19dvw] rounded-full bg-blue-5 opacity-15 blur-3xl md:block'></div>
       <div className='mt-8 flex flex-wrap items-center justify-center gap-4 pt-12'>
-        <img className='w-8 md:w-10 xl:w-auto' src={gea_icon} alt='gea_icon' />
-        <h3 className='m-0 text-6 font-black text-red-400 md:text-4xl xl:text-17'>
+        <h3 className='m-0 text-6 font-black text-blue-8 md:text-4xl xl:text-17'>
           Somos una gran comunidad
         </h3>
       </div>
-      <div className='flex flex-wrap justify-center justify-items-center gap-x-20 md:gap-14 xl:mt-14 xl:gap-x-32'>
-        {counters.map((counter) => (
-          <div
-            className='mt-6 flex flex-col items-center md:gap-7'
-            key={counter.id}
-          >
-            <img
-              alt={counter.text}
-              className='w-12 max-w-[114px] pt-3 md:w-[8dvw]'
-              src={counter.icon}
-            />
-            <h4
-              className='text-center text-10 font-black text-red-500 md:text-5xl xl:text-7xl'
-              id='counter'
-            >
-              {counter.number}
-            </h4>
-            <p className='font-black text-blue-8 md:text-7 xl:text-11'>
-              {counter.text}
-            </p>
-          </div>
+      <div className='flex flex-wrap justify-around gap-x-20 px-5 md:gap-14 md:px-16 xl:mt-14 xl:gap-x-0 2xl:px-24'>
+        {currentCounters.map((counter, index) => (
+          <React.Fragment key={counter.id}>
+            <div className='mt-6 flex flex-col items-center md:gap-7'>
+              <div className='w-12 max-w-[114px] pt-3 md:w-[8dvw]'>
+                {React.createElement(counter.icon, {
+                  className: 'w-full h-full bg-white fill-none stroke-red-500'
+                })}
+              </div>
+              <h4
+                className='text-center text-10 font-black text-blue-8 md:text-5xl 2xl:text-7xl'
+                id='counter'
+              >
+                {counter.number}
+              </h4>
+              <p className='font-black text-blue-8 md:text-7 xl:text-11'>
+                {counter.text}
+              </p>
+            </div>
+            {index < currentCounters.length - 1 && (
+              <div className='hidden w-[1px] bg-gray-400 md:block md:h-72'></div>
+            )}
+          </React.Fragment>
         ))}
       </div>
     </section>
--- src/app/home/components/DataCommunity.tsx
+++ src/app/home/components/DataCommunity.tsx
@@ -28,20 +28,23 @@ export const DataComunity = () => {
       ))}
       <div
         className={
-          'relative flex flex-col items-center justify-center bg-black-8 bg-[url(https://firebasestorage.googleapis.com/v0/b/scrum-latam-imgs.appspot.com/o/Info%20Community%20component%2FAbstract%20textured%20backgound.png?alt=media&token=296561ab-a517-4fbf-873d-b3bde1a2feb8)] bg-cover bg-center text-[#FCFCFC] bg-blend-color-dodge md:items-start md:pl-[6dvw] md:text-blue-9 md:bg-blend-normal'
+          'relative flex flex-col items-center justify-center bg-[#082965] text-[#FCFCFC] md:items-start md:pl-[6dvw] md:text-blue-9 md:bg-blend-normal'
         }
       >
-        <h3 className='mb-0 text-[5.5dvw] font-bold text-[#FCFCFC] md:pb-7 md:text-start md:text-[2dvw] md:font-medium 2xl:text-[30px]'>
+        <h3 className='mb-0 text-[5.5dvw] font-bold text-[#FCFCFC] md:pb-7 md:text-start md:text-[45px] 2xl:text-[65px]'>
           Conecta, Aprende y Lidera
         </h3>
-        <p className='mt-0 w-9/12 text-center text-[3.5dvw] font-medium leading-[3.6dvw] text-[#FCFCFC] md:w-[60%] md:text-start md:text-[1.5dvw] md:font-normal md:leading-4 2xl:text-[25px]'>
+        <p className='mt-5 w-9/12 text-center text-[3.5dvw] font-medium leading-[3.6dvw] text-[#FCFCFC] md:w-[60%] md:text-start md:text-[18px] md:font-normal md:leading-4 2xl:text-[22px]'>
           Somos una comunidad <b className='font-bold'>LATAM </b>
           dedicada a la difusión y aprendizaje del marco de trabajo Scrum y
           otras metodologías ágiles. Nuestro objetivo es compartir
           conocimientos, experiencias y fomentar el desarrollo continuo de
-          nuestros miembros. Empoderamos a líderes de equipos en organizaciones,
-          promoviendo el crecimiento profesional y la adaptación a diversos
-          proyectos en el mundo de la Agilidad.
+          nuestros miembros.
+        </p>
+        <p className='mt-5 w-9/12 text-center text-[3.5dvw] font-medium leading-[3.6dvw] text-[#FCFCFC] md:w-[60%] md:text-start md:text-[18px] md:font-normal md:leading-4 2xl:text-[22px]'>
+          Empoderamos a líderes de equipos en organizaciones, promoviendo el
+          crecimiento profesional y la adaptación a diversos proyectos en el
+          mundo de la Agilidad.
         </p>
       </div>
     </div>
