6f3037de1c5194702f0ed136a65f3795dc42694e
Fernando Campellone <tomascampellone@gmail.com>
2025-07-21 05:53:49 -0300

    feat: redesign our history

---
+++ src/assets/NewChevronRight.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+interface ChevronProps {
+  className?: string
+  width?: number
+  height?: number
+}
+
+const NewChevronRight = ({
+  className,
+  width = 12,
+  height = 22
+}: ChevronProps) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox='0 0 12 22'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    className={className}
+  >
+    <path
+      d='M2 20L10 11L2 2'
+      stroke='currentColor'
+      strokeWidth='3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
+
+export default NewChevronRight
